2e055d1d1e12702a25c3cc5c57e54e7e2af71a6d
KangChou <kangchou666@gmail.com>
2022-04-11 17:46:31 +0800

    Add files via upload

---
+++ cpp_demo/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,23 @@
+#include <jni.h>
+#include <string>
+#include <cstddef>
+#include <typeinfo>
+#include <iostream>
+
+using namespace std;
+
+int ress(int x,int y){
+    return x+y;
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_cpp_1demo_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string strTest;
+    strTest = to_string(ress(1,2)) + " is a string.";
+    std::string hello = "Hello from C++"+strTest;
+
+    printf("x+y=%d\n",ress(1,2));
+    return env->NewStringUTF(hello.c_str());
+}
